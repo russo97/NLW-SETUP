048d661a7a37e137197e6191e2019a140fee69cc
Wanderson Silva <wandersonsilva768@gmail.com>
2023-01-16 22:04:06 -0300

    feat: importando e usando cors do Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+app.register(cors);
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany();
