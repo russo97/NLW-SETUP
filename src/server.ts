5db4fd677d8bc536140b2003c9435553ce471fe3
Wanderson Silva <wandersonsilva768@gmail.com>
2023-01-16 22:00:24 -0300

    feat: adição do banco de dados Prisma + SQLite / funcionamento da primeira rota

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app.listen({
+  port: 3333
+});
